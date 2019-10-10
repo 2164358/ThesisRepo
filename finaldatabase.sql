9a2d39525279e7b7eac3f92e350bce415ae35a58
Darvin Rae Fernandez <38721223+2164543@users.noreply.github.com>
2019-10-10 23:06:06 +0800

    added  table for ledger, deduction, reconciliation

--- finaldatabase.sql
+++ finaldatabase.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2019 at 10:46 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.1.28
+-- Generation Time: Oct 10, 2019 at 05:01 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,15 +25,110 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cancel`
+--
+
+CREATE TABLE `cancel` (
+  `cancel_order` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `cancel_id` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deduction`
+--
+
+CREATE TABLE `deduction` (
+  `menu_name` varchar(150) NOT NULL,
+  `quantity` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `inventory`
 --
 
 CREATE TABLE `inventory` (
-  `ID` int(100) NOT NULL,
-  `Item_Name` varchar(45) NOT NULL,
-  `Category` enum('Wet Goods','Dry Goods','Groceries','Can Goods') NOT NULL,
-  `Quantity` int(11) NOT NULL,
-  `Unit_of_measurement` varchar(100) NOT NULL
+  `id` int(100) NOT NULL,
+  `itemname` varchar(45) NOT NULL,
+  `category` char(45) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `unitofmeasurement` char(45) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `itemname`, `category`, `quantity`, `unitofmeasurement`) VALUES
+(1, 'Carrots', 'Dry Goods', 10, 'g'),
+(2, 'patrick', 'pogi', 12, 'pound');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ledger`
+--
+
+CREATE TABLE `ledger` (
+  `item_name` varchar(50) NOT NULL,
+  `uom` int(50) NOT NULL,
+  `balance` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `desciption` varchar(150) NOT NULL,
+  `menu_id` int(10) NOT NULL,
+  `menu_name` varchar(45) NOT NULL,
+  `quantity` int(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reconcilation`
+--
+
+CREATE TABLE `reconcilation` (
+  `item_name` varchar(150) NOT NULL,
+  `inventory_quantity` int(50) NOT NULL,
+  `current_quantity` int(50) NOT NULL,
+  `remarks` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `returns`
+--
+
+CREATE TABLE `returns` (
+  `return_id` int(10) NOT NULL,
+  `order_table` int(40) NOT NULL,
+  `return_order` varchar(50) NOT NULL,
+  `quantity_order` int(40) NOT NULL,
+  `remarks` varchar(150) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spoilage`
+--
+
+CREATE TABLE `spoilage` (
+  `spoilage_id` int(10) NOT NULL,
+  `item_name` varchar(150) NOT NULL,
+  `category` varchar(150) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -41,20 +136,68 @@ CREATE TABLE `inventory` (
 --
 
 --
+-- Indexes for table `cancel`
+--
+ALTER TABLE `cancel`
+  ADD PRIMARY KEY (`cancel_id`);
+
+--
 -- Indexes for table `inventory`
 --
 ALTER TABLE `inventory`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`menu_id`);
+
+--
+-- Indexes for table `returns`
+--
+ALTER TABLE `returns`
+  ADD PRIMARY KEY (`return_id`);
+
+--
+-- Indexes for table `spoilage`
+--
+ALTER TABLE `spoilage`
+  ADD PRIMARY KEY (`spoilage_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `cancel`
+--
+ALTER TABLE `cancel`
+  MODIFY `cancel_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `menu_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `returns`
+--
+ALTER TABLE `returns`
+  MODIFY `return_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `spoilage`
+--
+ALTER TABLE `spoilage`
+  MODIFY `spoilage_id` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
